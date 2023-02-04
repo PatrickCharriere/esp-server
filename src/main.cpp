39743f706fe05c1ccdb8418b9e9bfdb79e446be0
Patrick Charriere <patrick.charriere.perso@gmail.com>
2023-02-05 08:41:52 +1100

    Cleanup functions and declarations

--- src/main.cpp
+++ src/main.cpp
@@ -15,13 +15,18 @@ using namespace std;
 #include <DallasTemperature.h>
 
 
+///////////////////////////////////////////////////////////////////////// e-MAIL
+// #define SMTP_HOST "smtp.gmail.com"
+// #define SMTP_PORT 465
+// #define AUTHOR_EMAIL "smarthome.205.marlborough@gmail.com"
+// #define AUTHOR_PASSWORD "mqlskdjf"
+// #define RECIPIENT_EMAIL "jimmy_angelmp@hotmail.com"
+// SMTPSession smtp;
+// void smtpCallback(SMTP_Status status);
+
 
 ///////////////////////////////////////////////////////////////////////// GPIOs
-#define WiFi_LED_PIN 4
-#define PROCESSING_LED_PIN 0
-#define STATUS_OK_LED_PIN 2
-#define ERROR_LED_PIN 15
-#define BUZZER_PIN 5
+#define WiFi_LED_PIN 2
 #define FAN_PIN 18
 
 
@@ -32,30 +37,24 @@ using namespace std;
 #define SCREEN_HEIGHT 64 // Display height (px)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
+
 ///////////////////////////////////////////////////////////////////////// WiFi
 const char* ssid     = "WiFi-14FE";
 const char* password = "205.easternStar";
-enum WIFI_STATUS { CONNECTING, CONNECTED };
-WIFI_STATUS wifiStatus = CONNECTING;
-enum MODULE_STATUS { GOOD, KO };
-MODULE_STATUS moduleStatus = GOOD;
-enum MODULE_PROCESSING { PROCESSING, INACTIVE };
-MODULE_PROCESSING moduleProcessing = INACTIVE;
 
 
 ///////////////////////////////////////////////////////////////////////// WEB SERVER
 AsyncWebServer server(80);
-// Asynchronous processes
 using namespace reactesp;
 ReactESP app;
-// Variable to store the HTTP req  uest
 String header;
+
 
 ///////////////////////////////////////////////////////////////////////// PWM
 int dutyCycle = 0;
-const int redLedPin = 12;  // RED
-const int greenLedPin = 13;  // GREEN
-const int blueLedPin = 14;  // BLUE
+const int redLedPin = 12;
+const int greenLedPin = 13;
+const int blueLedPin = 14;
 const int freq = 1000;
 const int redChannel = 0;
 const int greenChannel = 1;
@@ -72,29 +71,27 @@ const char* PARAM_MESSAGE = "rgb";
 
 
 ///////////////////////////////////////////////////////////////////////// INTERNET TIME
-// Define NTP Client to get time
 WiFiUDP ntpUDP;
 NTPClient timeClient(ntpUDP);
-// Variables to save date and time
 String formattedTime;
 String dayStamp;
 String timeStamp;
+void forceTimeUpdate(void) {
+  timeClient.forceUpdate();
+}
 void initialiseTime(void) {
-    // Initialize a NTPClient to get time
+  
     timeClient.begin();
-    // Set offset time in seconds to adjust for the timezone
-    // GMT +1 = 3600
-    // GMT +10 = 36000
-    // GMT -1 = -3600
-    // GMT 0 = 0
+    // To adjust timezone / GMT +1 = 3600 / GMT 0 = 0
     timeClient.setTimeOffset(39600);
+    forceTimeUpdate();
+
 }
 void getTime(void) {
-  timeClient.forceUpdate();
-  //format hh:mm:ss
-  formattedTime = timeClient.getFormattedTime();
-  //Serial.println(formattedTime);
+  
+  formattedTime = timeClient.getFormattedTime(); //format hh:mm:ss
   return;
+
 }
 
 
@@ -138,9 +135,7 @@ void initFileSytem(void) {
   } else {
     Serial.println("SPIFFS mounted");
   }
-  listAllFiles();
 }
-
 
 
 ///////////////////////////////////////////////////////////////////////// TEMPERATURE
@@ -154,9 +149,6 @@ String temperatureString = "";
 const int intervalSecControlTemperature = 60;
 
 
-
-
-
 void initDebug() {
   // Serial port for debugging purposes
   Serial.begin(115200);
@@ -164,16 +156,18 @@ void initDebug() {
 }
 
 bool wifiConnect() {
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  wifiStatus = CONNECTING;
 
+  WiFi.begin(ssid, password);
+
+  Serial.println("Connecting to WiFi..");
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.println("Connecting to WiFi..");
+    Serial.print(".");
+    delay(200);
   }
   Serial.println(String("Connected to WiFi ") + String(ssid));
-  wifiStatus = CONNECTED;
+  digitalWrite(WiFi_LED_PIN, HIGH);
   return 0;
+
 }
 
 void updateTemperature() {
@@ -190,69 +184,79 @@ void updateTemperature() {
 
 }
 
-void startAsyncProcesses() {
-  bool buzz = true;
+void updateLedColor(AsyncWebServerRequest *request) {
+  String colors[3];
 
-  // LED blinking processes
-  app.onRepeat(100, [] () {
+  Serial.println("RGB request ___");
 
-    switch (wifiStatus) {
-      case CONNECTING:
-        static bool state = LOW;
-        digitalWrite(WiFi_LED_PIN, state = !state);
-        break;
-      case CONNECTED:
-        digitalWrite(WiFi_LED_PIN, HIGH);
-        break;
-      default:
-        digitalWrite(WiFi_LED_PIN, LOW);
-        break;
-    }
-    switch (moduleStatus) {
-      case GOOD:
-        digitalWrite(STATUS_OK_LED_PIN, HIGH);
-        digitalWrite(ERROR_LED_PIN, LOW);
-        break;
-      default:
-        digitalWrite(STATUS_OK_LED_PIN, LOW);
-        digitalWrite(ERROR_LED_PIN, HIGH);
-        break;
-    }
-    switch (moduleProcessing) {
-      case PROCESSING:
-        static bool processingState = LOW;
-        digitalWrite(PROCESSING_LED_PIN, processingState = !processingState);
-        break;
-      default:
-        digitalWrite(PROCESSING_LED_PIN, LOW);
-        break;
-    }
+  if (request->hasParam(PARAM_MESSAGE)) {
+    String paramString = request->getParam(PARAM_MESSAGE)->value();
 
-  });
-
-  app.onRepeat(200, [] () {
+    colors[0] = paramString.substring(0, paramString.indexOf(','));
+    colors[1] = paramString.substring(paramString.indexOf(',')+1, paramString.lastIndexOf(','));
+    colors[2] = paramString.substring(paramString.lastIndexOf(',')+1, paramString.length());
     
-    static bool buzzing = false;
+    Serial.println(colors[0]);
+    Serial.println(colors[1]);
+    Serial.println(colors[2]);
+    ledcWrite(redChannel, 255-colors[0].toInt());
+    ledcWrite(greenChannel, 255-colors[1].toInt());
+    ledcWrite(blueChannel, 255-colors[2].toInt());
+
+  } else {
+      Serial.println("No params found");
+  }
+  request->send_P(200, "text/plain", "RGB request received by ESP32");
+
+}
+
+void setTemperatureParamsByWeb(AsyncWebServerRequest *request) {
+
+  Serial.println("Update Temperature params web request ___");
+
+  if (request->hasParam(PARAM_MESSAGE)) {
+    String paramString = request->getParam(PARAM_MESSAGE)->value();
+
+    paramString.substring(0, paramString.indexOf(','));
+    
+    Serial.println(paramString);
+  } else {
+      Serial.println("No params found");
+  }
+  request->send_P(200, "text/plain", "Update Temperature params request received");
+
+}
+
+void setNetworkParamsByWeb(AsyncWebServerRequest *request) {
+
+  Serial.println("Update Network params web request ___");
+
+  if (request->hasParam(PARAM_MESSAGE)) {
+    String paramString = request->getParam(PARAM_MESSAGE)->value();
+
+    paramString.substring(0, paramString.indexOf(','));
+    
+    Serial.println(paramString);
+  } else {
+      Serial.println("No params found");
+  }
+  request->send_P(200, "text/plain", "Update Network params request received");
+
+}
+
+void monitorTemperature(void) {
+
     static float previousTempWarning = false;
     static bool temperatureHighWarning = false;
 
-    // Temperature monitoring process
     if( (!temperatureHighWarning) && (temperatureC >= temperatureHighThresold)) {
-
 
       Serial.println("Temperature HIGH Warning");
       temperatureHighWarning = true;
-      // if(buzzing) {
-      //   noTone(BUZZER_PIN);
-      // } else {
-      //   tone(BUZZER_PIN, NOTE_A4);
-      // }
-      // buzzing = !buzzing;
 
     } else if ( (temperatureHighWarning) && (temperatureC <= temperatureLowThresold)) {
 
       Serial.println("Temperature LOW");
-      //noTone(BUZZER_PIN);
       temperatureHighWarning = false;
       
     }
@@ -268,42 +272,110 @@ void startAsyncProcesses() {
       }
       
     }
-      
-  });
 
-  app.onRepeat(1000, [] () {
-    //get time every second
-    getTime();
-
-    //refresh screen every second
-    display.setTextSize(5);
-    display.setCursor(5, 5);
-    display.clearDisplay();
-    display.println(temperatureString);
-    display.setTextSize(2);
-    display.setCursor(15, 46);
-    display.println(formattedTime);
-    display.display();
-  });
-
-  app.onRepeat(intervalSecControlTemperature * 1000, [] () {
-    updateTemperature();
-  });
 }
 
+void refreshDisplay() {
+
+  //refresh screen every second
+  display.setTextSize(5);
+  display.setCursor(5, 5);
+  display.clearDisplay();
+  display.println(temperatureString);
+  display.setTextSize(2);
+  display.setCursor(15, 46);
+  display.println(formattedTime);
+  display.display();
+
+}
+
+/*void smtpCallback(SMTP_Status status){
+  
+  Serial.println(status.info());
+
+  
+  if (status.success()){
+    Serial.println("----------------");
+    ESP_MAIL_PRINTF("Message sent success: %d\n", status.completedCount());
+    ESP_MAIL_PRINTF("Message sent failled: %d\n", status.failedCount());
+    Serial.println("----------------\n");
+    struct tm dt;
+
+    for (size_t i = 0; i < smtp.sendingResult.size(); i++){
+      
+      SMTP_Result result = smtp.sendingResult.getItem(i);
+      time_t ts = (time_t)result.timestamp;
+      localtime_r(&ts, &dt);
+
+      ESP_MAIL_PRINTF("Message No: %d\n", i + 1);
+      ESP_MAIL_PRINTF("Status: %s\n", result.completed ? "success" : "failed");
+      ESP_MAIL_PRINTF("Date/Time: %d/%d/%d %d:%d:%d\n", dt.tm_year + 1900, dt.tm_mon + 1, dt.tm_mday, dt.tm_hour, dt.tm_min, dt.tm_sec);
+      ESP_MAIL_PRINTF("Recipient: %s\n", result.recipients.c_str());
+      ESP_MAIL_PRINTF("Subject: %s\n", result.subject.c_str());
+    }
+    Serial.println("----------------\n");
+  }
+}
+
+void setupEmailParameters(void) {
+
+  smtp.debug(1);
+
+  smtp.callback(smtpCallback);
+
+  ESP_Mail_Session session;
+
+  session.server.host_name = SMTP_HOST;
+  session.server.port = SMTP_PORT;
+  session.login.email = AUTHOR_EMAIL;
+  session.login.password = AUTHOR_PASSWORD;
+  session.login.user_domain = "";
+
+  SMTP_Message message;
+
+  message.sender.name = "ESP";
+  message.sender.email = AUTHOR_EMAIL;
+  message.subject = "ESP Test Email";
+  message.addRecipient("Sara", RECIPIENT_EMAIL);
+
+  String htmlMsg = "<div style=\"color:#2f4468;\"><h1>Hello Jaime!</h1><p>It's too hot in here, I just started the fan to cool down the marine fish tank ;)</p><p>Have a good day</p><p>Smart home</p></div>";
+  message.html.content = htmlMsg.c_str();
+  message.html.content = htmlMsg.c_str();
+  message.text.charSet = "us-ascii";
+  message.html.transfer_encoding = Content_Transfer_Encoding::enc_7bit;
+
+  if (!smtp.connect(&session))
+    return;
+
+  if (!MailClient.sendMail(&smtp, &message))
+    Serial.print("Error sending Email, ");
+    Serial.println(smtp.errorReason());
+
+}*/
+
+void startAsyncProcesses() {
+
+  //Every second
+  app.onRepeat(1000, [] () {
+    getTime();
+    monitorTemperature();
+    refreshDisplay();
+  });
+
+  app.onRepeat(intervalSecControlTemperature * 1000, [] () { updateTemperature(); });
+
+  //Every Day
+  app.onRepeat(24 * 60 * 60 * 1000, [] () { forceTimeUpdate(); });
+
+}
 
 void setup() {
   
   Wire.begin();
-  pinMode(BUZZER_PIN, OUTPUT);
+
   pinMode(WiFi_LED_PIN, OUTPUT);
-  pinMode(PROCESSING_LED_PIN, OUTPUT);
-  pinMode(STATUS_OK_LED_PIN, OUTPUT);
-  pinMode(ERROR_LED_PIN, OUTPUT);
   pinMode(FAN_PIN, OUTPUT);
   digitalWrite(FAN_PIN, HIGH); //Disable at start
-  moduleStatus = GOOD;
-  moduleProcessing = INACTIVE;
 
   // Start the async process
   startAsyncProcesses();
@@ -312,15 +384,11 @@ void setup() {
   initDebug();
   wifiConnect();
 
-  initialiseTime();
-  getTime();
   initFileSytem();
 
-  // configure LED PWM functionalitites
   ledcSetup(redChannel, freq, resolution);
   ledcSetup(greenChannel, freq, resolution);
   ledcSetup(blueChannel, freq, resolution);
-  // attach the channel to the GPIO to be controlled
   ledcAttachPin(redLedPin, redChannel);
   ledcAttachPin(greenLedPin, greenChannel);
   ledcAttachPin(blueLedPin, blueChannel);
@@ -328,39 +396,11 @@ void setup() {
   // Print ESP32 Local IP Address
   Serial.println(WiFi.localIP());
 
-  
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html");
-  });
-  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/temp.txt");
-  });
-  server.on("/color", HTTP_GET, [](AsyncWebServerRequest *request){
-    String colors[3];
-
-      Serial.println("RGB request ____________________________________ ");
-
-      if (request->hasParam(PARAM_MESSAGE)) {
-        String paramString = request->getParam(PARAM_MESSAGE)->value();
-
-        colors[0] = paramString.substring(0, paramString.indexOf(','));
-        colors[1] = paramString.substring(paramString.indexOf(',')+1, paramString.lastIndexOf(','));
-        colors[2] = paramString.substring(paramString.lastIndexOf(',')+1, paramString.length());
-        
-        Serial.println(colors[0]);
-        Serial.println(colors[1]);
-        Serial.println(colors[2]);
-        ledcWrite(redChannel, 255-colors[0].toInt());
-        ledcWrite(greenChannel, 255-colors[1].toInt());
-        ledcWrite(blueChannel, 255-colors[2].toInt());
-
-      } else {
-          Serial.println("No params found");
-      }
-      request->send_P(200, "text/plain", "RGB request received by ESP32");
-
-  });
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){ request->send(SPIFFS, "/index.html"); });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){ request->send(SPIFFS, "/temp.txt"); });
+  server.on("/color", HTTP_GET, [](AsyncWebServerRequest *request) { updateLedColor(request); } );
+  server.on("/setTemperatureParams", HTTP_GET, [](AsyncWebServerRequest *request) { setTemperatureParamsByWeb(request); } );
+  server.on("/setNetworkParams", HTTP_GET, [](AsyncWebServerRequest *request) { setNetworkParamsByWeb(request); } );
     
   Serial.println("Start the DS18B20 sensor");
   sensors.begin();
@@ -379,12 +419,14 @@ void setup() {
   display.clearDisplay();
   display.setTextColor(WHITE);
 
+  initialiseTime();
+  getTime();
   updateTemperature();
+
+  //setupEmailParameters();
+
 }
 
 void loop(){
   app.tick();
 }
-
-
-
